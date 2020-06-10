0694c43cf5a0f9bf67c55705ec05c2b8d4761da0
psanchez <psanchez@plus54.com>
2020-06-10 19:36:54 -0300

    Ejemplo sencillo de GET y POST ProductoDTO

---
+++ ClaseServiciosWeb/Controllers/ProductosController.cs
@@ -0,0 +1,27 @@
+﻿using ClaseServiciosWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClaseServiciosWeb.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        public List<ProductoDTO> Get()
+        {
+            List<ProductoDTO> productos = new List<ProductoDTO>();
+            productos.Add(new ProductoDTO() { IdProducto = 1, Nombre = "Pelota", Precio = 5000 });
+            productos.Add(new ProductoDTO() { IdProducto = 2, Nombre = "Camiseta", Precio = 7000 });
+            productos.Add(new ProductoDTO() { IdProducto = 3, Nombre = "Botines", Precio = 4000 });
+            return productos;
+        }
+
+        public string Post(ProductoDTO prod)
+        {
+            return $"Producto {prod.Nombre} agregado exitosamente";
+        }
+    }
+}
---
+++ ClaseServiciosWeb/Models/ProductoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClaseServiciosWeb.Models
+{
+    public class ProductoDTO
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
