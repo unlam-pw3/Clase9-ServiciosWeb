d265bae1b6d84bdcfdaf1c65245915e81341d4b1
psanchez <psanchez@plus54.com>
2020-06-10 22:30:11 -0300

    agregando consumo de WebApi usando WebClient desde servidor

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
@@ -15,6 +20,40 @@ namespace Web.Controllers
             var productos = client.ObtenerTodos();
 
             return View(productos);
+        }
+
+        public ActionResult ListaDesdeWebApi()
+        {
+            List<ProductoDTO> productosDesdeWebApi = GetProductos();
+            return View(productosDesdeWebApi);
+        }
+
+        public ActionResult ListaDesdeWebApiPorJavascript()
+        {
+            return View();
+        }
+
+        private List<ProductoDTO> GetProductos()
+        {
+            var url = $"https://localhost:44306/api/productos";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (Stream strReader = response.GetResponseStream())
+                {
+                    if (strReader == null) return new List<ProductoDTO>();
+                    using (StreamReader objReader = new StreamReader(strReader))
+                    {
+                        string responseBody = objReader.ReadToEnd();
+                        var result = JsonConvert.DeserializeObject<List<ProductoDTO>>(responseBody);
+                        return result;
+                    }
+                }
+            }
         }
     }
 }
---
+++ Web/Models/ProductoDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class ProductoDTO
+    {
+        public int IdProduct { get; set; }
+        public string Nombre { get; set; }
+    }
+}
