9cd486b9cab697e7ce19931259f62e4d08a204e0
psanchez <psanchez@plus54.com>
2020-06-10 22:58:56 -0300

    agregando consumir Web api desde javascript, tira error de CORS

---
+++ Web/js/productos.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    var tblProductos = $('#tblProductos');
+
+    $('#btnRefrescar').click(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'https://localhost:44306/api/productos',
+            dataType: 'json',
+            success: function (data) {
+
+                tblProductos.empty();
+                $.each(data, function (index, val) {
+                    tblProductos.append('<tr><td>' + val.Nombre + '</td></tr>');
+                });
+            }
+
+        });
+
+    });
+
+});  
